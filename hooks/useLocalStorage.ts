ee0792d4cfdd8f31fe2714f771d72dba47f82a62
comitatopriv-ux <comitatopriv@gmail.com>
2025-10-20 09:29:56 +0200

    Update useLocalStorage.ts

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -4,7 +4,8 @@ import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 function useLocalStorage<T>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>] {
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
-      const item = window.localStorage.getItem(key);
+      if (typeof window === "undefined") return [initialValue, () => {}];
+const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
       console.error(error);
